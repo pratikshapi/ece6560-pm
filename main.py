177e62835941e6a19638443befe6e2681ff14869
pratiksha <paipratisha8@gmail.com>
2024-04-22 23:16:22 -0400

    cleanup

--- main.py
+++ main.py
@@ -44,38 +44,6 @@ def plot_results(original, noisy, denoised, noise_labels, denoise_labels):
     plt.tight_layout()
     plt.show()
 
-
-# def plot_results(original, noisy, denoised, noise_labels, denoise_labels):
-#     plt.figure(figsize=(15, 10))
-#     num_noisy = len(noisy)
-#     cols = num_noisy + 1  # For noisy images and the original image
-
-#     # First row: noisy images first, then the original image
-#     for i, img in enumerate(noisy, start=1):
-#         plt.subplot(5, cols, i)
-#         plt.imshow(img, cmap='gray')
-#         plt.title(noise_labels[i-1])
-#         plt.axis('off')
-
-#     # Place the original image last in the first row
-#     plt.subplot(5, cols, cols)
-#     plt.imshow(original, cmap='gray')
-#     plt.title("Original")
-#     plt.axis('off')
-
-#     # Subsequent rows: denoised images for each method
-#     row_offset = cols
-#     for d_index, denoised_group in enumerate(denoised):
-#         for i, img in enumerate(denoised_group, start=row_offset + 1):
-#             plt.subplot(5, cols, i)
-#             plt.imshow(img, cmap='gray')
-#             plt.title(f"{denoise_labels[d_index]}\n{noise_labels[(i - row_offset - 1) % num_noisy]}")
-#             plt.axis('off')
-#         row_offset += cols
-
-#     plt.tight_layout()
-#     plt.show()
-
 def process_image(image_path, noise_funcs, noise_labels, denoise_funcs, denoise_labels, display=True):
     original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     if original_image is None:
@@ -105,32 +73,6 @@ def process_image(image_path, noise_funcs, noise_labels, denoise_funcs, denoise_
     if display:
         plot_results(original_image, noisy_images, denoised_images, noise_labels, denoise_labels)
 
-# def process_image(image_path, noise_funcs, noise_labels, denoise_funcs, denoise_labels, display=True):
-#     original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-#     noisy_images = []
-    
-#     # Create and save noisy images
-#     for func, label in zip(noise_funcs, noise_labels):
-#         noisy = func(original_image.copy())
-#         noisy_images.append(noisy)
-#         cv2.imwrite(f'noised/{label.replace(" ", "_").lower()}.png', noisy)
-    
-#     # Create and save denoised images
-#     denoised_images = []
-#     for noisy, n_label in zip(noisy_images, noise_labels):
-#         for func, d_label in zip(denoise_funcs, denoise_labels):
-#             denoised = func(noisy.copy())
-#             denoised_images.append(denoised)
-#             filename = f'{n_label.replace(" ", "_").lower()}_with_{d_label.replace(" ", "_").lower()}.png'
-#             cv2.imwrite(f'denoised/{filename}', denoised)
-
-    
-#     if display:
-#         plot_results(original_image, noisy_images, denoised_images, noise_labels, denoise_labels)
-
-#     return noisy_images, denoised_images
-
-
 def main():
     ensure_dir('noised')
     ensure_dir('denoised')
@@ -146,34 +88,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# ensure_dir('noised')
-# ensure_dir('denoised')
-
-# def main():
-#     image_path = 'images/dog.jpg'
-#     noise_funcs = [
-#         lambda img: add_salt_pepper_noise(img, prob=0.05),  # Replacing speckle noise
-#         lambda img: add_gaussian_noise(img, var=0.05)  # Assuming Gaussian noise remains the same
-#     ]
-#     noise_labels = ['Salt & Pepper Noise', 'Gaussian Noise']  # Update labels accordingly
-
-    
-#     denoise_funcs = [
-#         lambda img: wavelet_denoising(img, level=3),
-#         lambda img: gaussian_denoise(img, sigma=1.0),
-#         lambda img: anisodiff_f1(img, steps=50, K=0.1),
-#         lambda img: anisodiff_f2(img, steps=50, K=0.1)
-#     ]
-#     denoise_labels = [
-#         'Wavelet Denoising',
-#         'Gaussian Denoising',
-#         'Perona-Malik F1 Denoising',
-#         'Perona-Malik F2 Denoising'
-#     ]
-
-#     process_image(image_path, noise_funcs, noise_labels, denoise_funcs, denoise_labels)
-
-# if __name__ == '__main__':
-#     main()
